f4d3de602fa651b996121e5d7ada07e190d8f556
LRomanTomas <tomasroman07@hotmail.com>
2025-10-10 18:31:59 -0300

    cambios

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -124,28 +124,24 @@ export class HomePage implements OnInit {
   }
 
   async escanearQrEntrada() {
-    this.router.navigate(['/encuestas-espera'], {
-        queryParams: { clienteId: this.clienteId, tienePermiso : true, yaRegistrado: true, qrValido : true, userId : this.userId}
+    const qr = await this.qr.scanQr();
+    if (!qr) return;
+
+    const res = await this.qr.procesarQrCliente(
+      qr,
+      this.clienteId ?? undefined
+    );
+
+    if (res.error) {
+      this.mostrarToast(res.error, 'danger');
+      return;
+    }
+
+    if (res.permiso && qr.startsWith('INGRESO')) {
+
+      this.router.navigate(['/encuestas-espera'], {
+        queryParams: { clienteId: this.clienteId, tienePermiso : true, yaRegistrado: !!res.yaRegistrado, qrValido : true, userId : this.userId}
       });
     }
-  //   const qr = await this.qr.scanQr();
-  //   if (!qr) return;
-
-  //   const res = await this.qr.procesarQrCliente(
-  //     qr,
-  //     this.clienteId ?? undefined
-  //   );
-
-  //   if (res.error) {
-  //     this.mostrarToast(res.error, 'danger');
-  //     return;
-  //   }
-
-  //   if (res.permiso && qr.startsWith('INGRESO')) {
-
-  //     this.router.navigate(['/encuestas-espera'], {
-  //       queryParams: { clienteId: this.clienteId, tienePermiso : true, yaRegistrado: !!res.yaRegistrado, qrValido : true, userId : this.userId}
-  //     });
-  //   }
-  // }
+  }
 }
